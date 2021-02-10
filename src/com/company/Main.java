84fb24c4b6cb817e1b4e94c91cbc0171d453b673
SvitlanaBilous87 <s_vitlanka@ukr.net>
2021-02-10 14:39:02 +0200

    added comparing numbers (which is closer to 10)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,10 +13,12 @@ public class Main {
         System.out.println("");
         System.out.println("ЗАВДАННЯ №4");
         printfrom90();
-
+        System.out.println("");
+        System.out.println("ЗАВДАННЯ №5");
+        compare10();
     }
 
-    private static void days() {
+    public static void days() {
             System.out.println("Привіт! Давай перевірим, що на сьогодні заплановано :)");
             System.out.println("Який сьогодні день тижня? Напишіть українською мовою:");
             Scanner sc = new Scanner(System.in);
@@ -58,19 +60,63 @@ public class Main {
                 }
             }
         }
-    private static void print55el() {
+    public static void print55el() {
         int j = 1;
         for (int i = 0; i < 55; i++) {
             System.out.println(j);
             j = j + 2;
         }
     }
-    private static void printfrom90() {
+    public static void printfrom90() {
         int j=90;
         do {
             System.out.println(j);
             j = j-5;
         } while (j>=0);
+    }
+    public static double getNumber() {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextDouble()) {
+            return sc.nextDouble();
+        }
+        else {
+            System.out.println("Помилка введення даних! Потрібно ввести числове значення: ");
+            return getNumber();
+        }
+    }
+    public static void compare10() {
+        System.out.println("Порівняємо 2 числа і визначимо, яке з них ближче до 10.");
+        System.out.println("Введіть число 1: ");
+        double a1 = getNumber();
+        System.out.println("Введіть число 2: ");
+        double a2 = getNumber();
+        if (a1 == a2) {
+            System.out.println("Немає розв'язку, бо числа рівні.");
+        } else if (a1 == 10 || a2 == 10) {
+            System.out.println("Немає розв'язку, бо одне з чисел і є 10.");
+        } else if (a1 == 10 && a2 == 10) {
+            System.out.println("Немає розв'язку, бо обидва числа і є 10.");
+        } else {
+            if (a1 > 10 && a2 > 10) {
+                if ((a1 - 10) > (a2 - 10)) {
+                    System.out.println("Число " + a2 + " ближче до 10, ніж " + a1);
+                } else if ((a1 - 10) < (a2 - 10)) {
+                    System.out.println("Число " + a1 + " ближче до 10, ніж " + a2);
+                }
+            } else if (a1 < 10 && a2 < 10) {
+                if ((10 - a1) > (10 - a2)) {
+                    System.out.println("Число " + a2 + " ближче до 10, ніж " + a1);
+                } else if ((10 - a1) < (10 - a2)) {
+                    System.out.println("Число " + a1 + " ближче до 10, ніж " + a2);
+                }
+            } else {
+                if (Math.abs(a1 - 10) > Math.abs(a2 - 10)) {
+                    System.out.println("Число " + a2 + " ближче до 10, ніж " + a1);
+                } else {
+                    System.out.println("Число " + a1 + " ближче до 10, ніж " + a2);
+                }
+            }
+        }
     }
 }
 
