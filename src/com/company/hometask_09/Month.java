351575b212361098a4858cc5d75615aa8ed4a019
SvitlanaBilous87 <s_vitlanka@ukr.net>
2021-03-01 13:12:32 +0200

    Corrections to "Season and month" enum task

--- src/com/company/hometask_09/Month.java
+++ src/com/company/hometask_09/Month.java
@@ -28,4 +28,97 @@ public enum Month {
     public Season getSeason() {
         return season;
     }
-}
+
+    public static boolean checkMonth(String someText) {
+        boolean b = true;
+        for (Month month1 : Month.values()) {
+            if (someText.equalsIgnoreCase(month1.name())) {
+                b = false;
+                break;
+            }
+            }
+        if (!b) {
+            System.out.println("Such month exists!");
+            return true;
+            }
+        else {
+            System.out.println("Such month doesn't exist ((");
+            return false;
+        }
+    }
+        public static void checkSeason(Month someMonth) {
+            System.out.println("It is in " + someMonth.getSeason().name() + ". Also " +
+                    someMonth.getSeason().name() + "'s months are:");
+            for (Month month1 : Month.values()) {
+                if (month1.getSeason().equals(someMonth.getSeason()) && month1 != someMonth) {
+                    System.out.println(month1.name());
+                }
+            }
+        }
+        public static void checkDays(Month someMonth) {
+            if (someMonth == Month.FEBRUARY) {
+                System.out.println(someMonth.name() + " consists of " + someMonth.getDays() +
+                        " days.There is no month with the same number of days.");
+            } else {
+                System.out.println(someMonth.name() + " consists of " + someMonth.getDays() + " days. Same number of days also have:");
+                for (Month month1 : Month.values()) {
+                    if (month1.getDays() == someMonth.getDays() && month1 != someMonth) {
+                        System.out.println(month1.name());
+                    }
+                }
+            }
+            if (someMonth == Month.FEBRUARY) {
+                System.out.println(someMonth.name() + " has the least number of days.");
+            } else {
+                System.out.println("Months with lower number of days:");
+                for (Month month1 : Month.values()) {
+                    if (month1.getDays() < someMonth.getDays()) {
+                        System.out.println(month1.name());
+                    }
+                }
+            }
+            if (someMonth.getDays() == 31) {
+                System.out.println(someMonth.name() + " has the highest number of days.");
+            } else {
+                System.out.println("Months with higher number of days:");
+                for (Month month1 : Month.values()) {
+                    if (month1.getDays() > someMonth.getDays()) {
+                        System.out.println(month1.name());
+                    }
+                }
+            }
+        }
+        public static void closeSeason(Month someMonth){
+                Season[] seasons = Season.values();
+                if (someMonth.getSeason().ordinal() == (seasons.length - 1)) {
+                    System.out.println("Next season is " + seasons[0].name() + ".");
+                } else {
+                    System.out.println("Next season is " + seasons[someMonth.getSeason().ordinal() + 1].name() + ".");
+                }
+                if (someMonth.getSeason().ordinal() == 0) {
+                    System.out.println("Previous season is " + seasons[seasons.length - 1].name() + ".");
+                } else {
+                    System.out.println("Previous season is " + seasons[someMonth.getSeason().ordinal() - 1].name() + ".");
+                }
+            }
+    public static void evenOrOddCheckDays(Month someMonth){
+        System.out.println("Months with even number of days: ");
+        for (Month month : Month.values()) {
+        if (month.getDays() % 2 == 0) {
+            System.out.println(month.name());
+        }
+    }
+        System.out.println("Months with odd number of days: ");
+        for (Month month : Month.values()) {
+        if (month.getDays() % 2 != 0) {
+            System.out.println(month.name());
+        }
+    }
+            if (someMonth.getDays() % 2 == 0) {
+                System.out.println(someMonth.name()+" has even number of days.");
+            }
+            else {
+                System.out.println(someMonth.name()+" has odd number of days.");
+            }
+        }
+    }
