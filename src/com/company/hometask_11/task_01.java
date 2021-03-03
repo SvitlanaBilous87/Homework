c54206eba5e6e071864f9670f6776fb5c95e0c6f
SvitlanaBilous87 <s_vitlanka@ukr.net>
2021-03-03 14:59:35 +0200

    "String tasks" corrected

--- src/com/company/hometask_11/task_01.java
+++ src/com/company/hometask_11/task_01.java
@@ -1,9 +1,8 @@
 package com.company.hometask_11;
 
-import java.util.Locale;
-
 public class task_01 {
     public static void main(String[] args) {
+
         String str = "I like Java !!!";
         System.out.println(str.charAt(str.length()-1));
         System.out.println(str.endsWith("!!!"));
@@ -14,5 +13,6 @@ public class task_01 {
         System.out.println(str.toUpperCase());
         System.out.println(str.toLowerCase());
         System.out.println(str.substring(0, str.indexOf("Java"))+str.substring(str.indexOf("Java")+"Java".length()));
+
     }
 }
