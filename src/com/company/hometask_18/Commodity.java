449e0e36a944990e0c2e6a66203155d5adfba99d
SvitlanaBilous87 <s_vitlanka@ukr.net>
2021-03-22 12:26:21 +0200

    "Commodity" task modified with anonymous classes

---
+++ src/com/company/hometask_18/Commodity.java
@@ -0,0 +1,106 @@
+package com.company.hometask_18;
+
+import java.util.Scanner;
+
+public class Commodity {
+
+        private String name;
+        private Integer length;
+        private Integer width;
+        private Double weight;
+
+    public Commodity(String name, Integer length, Integer width, Double weight) {
+        this.name = name;
+        this.length = length;
+        this.width = width;
+        this.weight = weight;
+    }
+
+    public Commodity() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter name:");
+        this.name = scan.next();
+        System.out.println("Enter length:");
+        try {
+            if (scan.hasNextInt()) {
+                length = scan.nextInt();
+                if (length > 0) {
+                    this.length = length;
+                    System.out.println("Enter width:");
+                    if (scan.hasNextInt()) {
+                        width = scan.nextInt();
+                        if (width > 0) {
+                            this.width = width;
+                            System.out.println("Enter weight:");
+                            if (scan.hasNextDouble()) {
+                                weight = scan.nextDouble();
+                                if (weight > 0) {
+                                    this.weight = weight;
+                                } else {
+                                    throw new IllegalArgumentException("Weight must be >0.");
+                                }
+                            } else {
+                                throw new IllegalArgumentException("Weight must be number.");
+                            }
+                        } else {
+                            throw new IllegalArgumentException("Width must be >0.");
+                        }
+                    } else {
+                        throw new IllegalArgumentException("Width must be integer number.");
+                    }
+                } else {
+                    throw new IllegalArgumentException("Length must be >0.");
+                }
+            }
+             else {
+                throw new IllegalArgumentException("Length must be integer number.");
+            }
+        }
+        catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Commodity{" +
+                "name='" + name +
+                "', length=" + length +
+                ", width=" + width +
+                ", weight=" + weight +
+                '}';
+    }
+
+}
