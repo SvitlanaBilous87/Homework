b37d3cdc77e91fb8bfb3af2a74c9f52fc51f084b
SvitlanaBilous87 <s_vitlanka@ukr.net>
2021-03-29 13:07:51 +0300

    "Serialization" task_3

--- src/com/company/hometask_20/Main.java
+++ src/com/company/hometask_20/Main.java
@@ -1,5 +1,9 @@
 package com.company.hometask_20;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,5 +13,14 @@ public class Main {
         Methods.serialize(employee,currentDir+"\\src\\com\\company\\hometask_20\\employeeOut.txt");
         System.out.println(Methods.deserialize(currentDir+"\\src\\com\\company\\hometask_20\\employeeOut.txt"));
 
+        List<Employee> employees = new ArrayList<>(Arrays.asList(employee));
+        employees.add(new Employee("Den", 5612, 999.99));
+        employees.add(new Employee("Hanna", 777, 1867.4));
+        employees.add(new Employee("Mary", 9262, 1290.87));
+
+        Methods.serialize(employees, currentDir+"\\src\\com\\company\\hometask_20\\employeeOut.txt");
+        List<Employee> employeesDeserialized = (List<Employee>) Methods.deserialize(currentDir+"\\src\\com\\company\\hometask_20\\employeeOut.txt");
+
+        System.out.println(employeesDeserialized);
     }
 }
